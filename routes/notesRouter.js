6d682498308fafadcc1e953d2d23b98c8f71574b
Elena Lushnikova <elushnikova.256@gmail.com>
2022-12-13 15:55:18 +0300

    feat: Show single note

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -19,4 +19,29 @@ notesRouter.get('/', async (req, res) => {
   res.json(result);
 });
 
+// GET /notes/:id
+notesRouter.get('/:id', async (req, res) => {
+  const result = {
+    title: `Заметка №${req.params.id}`,
+    error: null,
+    data: null,
+  };
+
+  const id = Number(req.params.id);
+  if (Number.isNaN(id)) {
+    result.error = 'ID должен быть числовой';
+    res.status(406).json(result);
+    return;
+  }
+
+  try {
+    result.data = await db.Note.findByPk(id);
+  } catch (error) {
+    result.error = error.message;
+    res.status(500);
+  }
+
+  res.json(result);
+});
+
 module.exports = notesRouter;
