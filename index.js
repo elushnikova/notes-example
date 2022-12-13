b2b702fc2796101e0515f4d6bc7ad973e5f1844a
Elena Lushnikova <elushnikova.256@gmail.com>
2022-12-13 15:01:00 +0300

    chore: Setup minimal web server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ title: "Анонимный блог", message: "Ведутся работы" });
+});
+
+app
+  .listen(PORT)
+  .on("error", (error) => {
+    console.error("Ошибка при запуске веб-сервера");
+    console.error(error.message);
+  })
+  .on("listening", () => {
+    console.log("Веб-сервер слушает порт", PORT);
+  });
