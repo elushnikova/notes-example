05dd856406c1ef5697fcde9b4c53496f267bd48e
Elena Lushnikova <elushnikova.256@gmail.com>
2022-12-14 09:36:33 +0300

    feat: Show Harold image at home page

--- views/HomePage.jsx
+++ views/HomePage.jsx
@@ -5,6 +5,7 @@ function HomePage({ title, appTitle }) {
   return (
     <Layout title={title} appTitle={appTitle}>
       <h2>{title}</h2>
+      <img src="/images/harold.jpg" width="300" alt="От Гарольда с любовью" />
     </Layout>
   );
 }
