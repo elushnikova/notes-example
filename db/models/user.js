e43572c6533b93edef9aa97410a3221a7fc83b2e
Elena Lushnikova <elushnikova.256@gmail.com>
2022-12-20 10:19:05 +0300

    feat: Add User model and migration

---
+++ db/models/user.js
@@ -0,0 +1,45 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {}
+  }
+
+  const attributes = {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    login: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    displayName: {
+      type: DataTypes.TEXT,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  };
+
+  const options = {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+  };
+
+  User.init(attributes, options);
+  return User;
+};
