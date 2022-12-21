a0d8b3982dfebf511549b1a54f2eb989187db254
Elena Lushnikova <elushnikova.256@gmail.com>
2022-12-21 11:59:08 +0300

    fix: Add Bootstrap styles

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -15,6 +15,12 @@ function Layout({ title, appTitle, children }) {
           Атрибут defer нужен, если работаем с DOM-деревом. Почему и как сделать иначе?
         */}
         <script src="/scripts/client.js" defer />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
       </head>
       <body>
         <header>
